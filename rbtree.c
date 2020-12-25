2f23c65ae9536a3a4851f1fa595c1fb16c9c8387
许继元 <490601115@qq.com>
2020-12-25 10:33:34 +0800

    feat: the interface implementation of the red-black tree

---
+++ rbtree.c
@@ -0,0 +1,10 @@
+/**
+ * @filename rbtree.c
+ * @description RBTree interface implementation
+ * @author 许继元
+ * @date 2020/12/18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "rbtree.h"
