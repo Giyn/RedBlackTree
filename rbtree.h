970c381f52c25b8aae5af6c7ccf756fc46b5c284
许继元 <490601115@qq.com>
2020-12-25 10:59:05 +0800

    feat: add macro definition and structure

--- rbtree.h
+++ rbtree.h
@@ -8,4 +8,23 @@
 #ifndef RBTREE_RBTREE_H
 #define RBTREE_RBTREE_H
 
+#define RED   0 /* 红色结点标志 */
+#define BLACK 1 /* 黑色结点标志 */
+
+typedef int Type;
+
+/* 红黑树的结点 */
+typedef struct RBTreeNode {
+    Type key;                  /* 关键字 */
+    unsigned char color;       /* 颜色 */
+    struct RBTreeNode *left;   /* 左孩子 */
+    struct RBTreeNode *right;  /* 右孩子 */
+    struct RBTreeNode *parent; /* 父结点 */
+} Node, *RBTree;
+
+/* 红黑树的根 */
+typedef struct rb_root {
+    Node *node;
+} RBRoot;
+
 #endif /* RBTREE_RBTREE_H */
