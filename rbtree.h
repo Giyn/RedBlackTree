a17cba0c64892789a3e26925d8331cae85b42add
许继元 <490601115@qq.com>
2020-12-25 10:34:43 +0800

    feat: the interface declaration of the red-black tree

---
+++ rbtree.h
@@ -0,0 +1,11 @@
+/**
+ * @filename rbtree.h
+ * @description RBTree interface declaration
+ * @author 许继元
+ * @date 2020/12/18
+ */
+
+#ifndef RBTREE_RBTREE_H
+#define RBTREE_RBTREE_H
+
+#endif /* RBTREE_RBTREE_H */
