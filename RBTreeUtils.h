349186a9fe8faec2b0aba5210ef0dd943eefbc1b
许继元 <490601115@qq.com>
2020-12-26 10:13:26 +0800

    perf: modify the parameter name

--- RBTreeUtils.h
+++ RBTreeUtils.h
@@ -27,9 +27,9 @@ RBTree searchNode(RBTree tree, RBTreeElemType x);
 RBTree createRBTreeNode(RBTreeElemType x, Node *parent, Node *left, Node *right);
 
 /* 红黑树结点左旋 */
-Status RBTreeLeftRotate(RBRoot *root, Node *x);
+Status RBTreeLeftRotate(RBRoot *root, Node *node);
 
 /* 红黑树结点右旋 */
-Status RBTreeRightRotate(RBRoot *root, Node *y);
+Status RBTreeRightRotate(RBRoot *root, Node *node);
 
 #endif /* RBTREEUTILS_H */
