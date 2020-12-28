c0ea6e335b8bc1f69cb24c68dc85b2cf619236bb
许继元 <490601115@qq.com>
2020-12-28 11:54:28 +0800

    refactor: move the binary tree interface

--- RBTreeUtils.h
+++ RBTreeUtils.h
@@ -8,21 +8,6 @@
 #ifndef RBTREEUTILS_H
 #define RBTREEUTILS_H
 
-/* 销毁二叉树 */
-Status destroyBinaryTree(RBTree tree);
-
-/* 前序遍历二叉树 */
-Status preorderBiTree(RBTree tree);
-
-/* 中序遍历二叉树 */
-Status inorderBiTree(RBTree tree);
-
-/* 后序遍历二叉树 */
-Status postorderBiTree(RBTree tree);
-
-/* 查找结点 */
-RBTree searchNode(RBTree tree, RBTreeElemType x);
-
 /* 创建红黑树结点 */
 RBTree createRBTreeNode(RBTreeElemType x, Node *parent, Node *left, Node *right);
 
